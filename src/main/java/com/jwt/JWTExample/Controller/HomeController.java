b11fe25cd04db04c4ee9e12fe1e8ccfcdf6705cf
Shah <shah-deepak.muneshwar@capgemini.com>
2023-07-10 02:56:54 +0530

    Cross origin annotation added

--- src/main/java/com/jwt/JWTExample/Controller/HomeController.java
+++ src/main/java/com/jwt/JWTExample/Controller/HomeController.java
@@ -3,6 +3,7 @@ package com.jwt.JWTExample.Controller;
 import com.jwt.JWTExample.Entity.User;
 import com.jwt.JWTExample.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin
 public class HomeController {
 
     @Autowired
